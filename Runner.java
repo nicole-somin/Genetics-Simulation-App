645fd3f545df3e15add42d4369f62f111dd3cca8
nicole-somin <nsomin123@gmail.com>
2022-10-25 17:21:04 -0700

    changes

--- Runner.java
+++ Runner.java
@@ -1,5 +1,14 @@
-public class Runner {
-    public static void main(String[] args){
-        
-    }
+import javax.swing.JFrame;
+
+public class Runner{
+	public static void main(String[] args){
+		JFrame fr = new JFrame("Genetics Simulation App");
+		Screen sc = new Screen();
+		
+		fr.add( sc );
+		fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		fr.pack();
+		fr.setVisible(true);
+	
+	}
 }
--- Screen.java
+++ Screen.java
@@ -1,3 +1,29 @@
-public class Screen {
+import javax.swing.JPanel;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Screen extends JPanel{
     
+    public Screen(){
+        setLayout(null);
+
+        setFocusable(true);
+    }
+
+    @Override
+	public Dimension getPreferredSize(){
+		return new Dimension(800,600);
+	}
+
+    @Override
+	public void paintComponent(Graphics g){
+        
+    }
 }
